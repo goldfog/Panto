0d273bd958aa1015256e810578b6b6232e13001e
Dmitry B <fogosony700@gmail.com>
2024-03-24 17:54:22 +0300

    modified file

--- js/main.js
+++ js/main.js
@@ -88,3 +88,17 @@ for (let btn of tabsBtns) {
 		swiper.update();
 	});
 }
+
+// Mobile nav
+
+const mobileNavOpenBtn = document.querySelector('#open-mobile-nav-btn');
+const mobileNavCloseBtn = document.querySelector('#close-mobile-nav-btn');
+const mobileNav = document.querySelector('#mobile-nav');
+
+mobileNavOpenBtn.onclick = function () {
+	mobileNav.classList.add("mobile-nav-wrapper--open");
+};
+
+mobileNavCloseBtn.onclick = function () {
+	mobileNav.classList.remove("mobile-nav-wrapper--open");
+};
